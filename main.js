2e8d2822ec2c243b5fd34685b70b76a22d66dab8
Mazharul Islam <swopnil420@gmail.com>
2022-05-26 06:43:43 +0600

    Building Chrome Extenison

---
+++ main.js
@@ -0,0 +1,24 @@
+const clickCount = document.querySelector('.clickWrapp h2 span');
+const clickPlus = document.querySelector('.clickPlus');
+const clickMinus = document.querySelector('.clickMinus');
+const clickReset = document.querySelector('.clickReset');
+
+let count = localStorage.getItem(window.location.hostname + 'count');
+clickCount.innerHTML = count;
+
+clickPlus.addEventListener('click', () => {
+	clickCount.innerHTML = ++count;
+	localStorage.setItem(window.location.hostname + 'count', count);
+});
+
+clickMinus.addEventListener('click', () => {
+	clickCount.innerHTML = --count;
+	localStorage.setItem(window.location.hostname + 'count', count);
+});
+
+clickReset.addEventListener('click', () => {
+	console.log('Hello');
+	count = 0;
+	clickCount.innerHTML = count;
+	localStorage.setItem(window.location.hostname + 'count', count);
+});
